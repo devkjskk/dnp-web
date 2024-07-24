6c0c79e70a15ef7009dba2194f0201d5ccffcc1e
devsocker <dev.kjskk@gmail.com>
2024-07-25 03:25:06 +0700

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "nextjs-admin-web-git-main-make-it-down.vercel.app"],
+    domains: [
+      "localhost",
+      "nextjs-admin-web-git-main-make-it-down.vercel.app",
+      "dnp.todly.io",
+    ],
   },
 };
 
